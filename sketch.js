5c5535b0d2ae024fbbeb68ae9a6ec9f5ad0acbf7
hiya29 <75164885+hiya29@users.noreply.github.com>
2021-09-03 13:10:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 var balloon,balloonImage1,balloonImage2;
-var database, Position;
+
+// create database and position variable here
+
 function preload(){
    bg =loadImage("cityImage.png");
    balloonImage1=loadAnimation("hotairballoon1.png");
@@ -15,7 +17,7 @@ function setup() {
 
   balloon=createSprite(250,450,150,150);
   balloon.addAnimation("hotAirBalloon",balloonImage1);
-  balloon.scale=0.5;
+  balloon.scale=0.5; 
 
   textSize(20); 
 }
@@ -24,25 +26,25 @@ function setup() {
 function draw() {
   background(bg);
 
-  if(keyDown(LEFT_ARROW)){ 
-    updateHeight(-10, 0);
+  if(keyDown(LEFT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    
+    //write code to move air balloon in left direction
+    updateHeight(-10,0)
   }
-  else if(keyDown(RIGHT_ARROW)){ 
-    updateHeight(10, 0);
+  else if(keyDown(RIGHT_ARROW)){
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    
+    //write code to move air balloon in right direction
+    updateHeight(+10,0)
   }
   else if(keyDown(UP_ARROW)){
-    updateHeight(0,-10);
-    balloon.addAnimation("hotAirBalloon",balloonImage2); 
-    balloon.scale=balloon.scale -0.01;
-  }
-  else if(keyDown(DOWN_ARROW)){ 
-    updateHeight(0, +10);
     balloon.addAnimation("hotAirBalloon",balloonImage2);
-    balloon.scale=balloon.scale +0.01;
+    //write code to move air balloon in up direction
+    updateHeight(0,-10)
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    updateHeight(0,+10)
   }
 
   drawSprites();
@@ -51,24 +53,23 @@ function draw() {
   textSize(25);
   text("**Use arrow keys to move Hot Air Balloon!",40,40);
 }
+function readHeight(data){
+var height=data.val()
+balloon.x=height.x
+balloon.y=height.y
 
+
+}
 function updateHeight(x,y){
 database.ref('balloon/height').set({
-'x': height.x + x ,
-"y": height.y + y
+'x' : height.x+x,
+'y' : height.y+y
+
+
+
+
+
 })
-}
 
-function readHeight(data){
-  height = data.val();
-  balloon.x = height.x;
-  balloon.y = height.y; 
-   
-  var balloonPosition = database.ref('balloon/height');
-  balloonPosition.on("value", readPosition, showError);
 
-} 
-
-function showError(){
-  console.log("Error in writing to the database")
 }
